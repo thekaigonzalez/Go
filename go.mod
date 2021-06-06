08683bf98587f2bba7200797c42fca6849f765be
thekaigonzalez <gkai70264@gmail.com>
2021-06-06 14:10:26 -0400

    Go test init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thekaigonzalez/Go
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,14 @@
+package main;
+
+
+import "fmt"
+
+
+
+func main() {
+	fmt.Printf("%s: cool\n", "kai");
+	var first string;
+	fmt.Scanln(&first);
+	fmt.Printf("input: %s\n", first); //First
+	
+}
